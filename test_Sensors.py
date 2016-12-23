0b97ae964fac58e73a94371b52c9962f4c40472b
Rangga <rangga.p@gmail.com>
2016-12-23 13:50:36 +0000

    add second sensor

--- test_Sensors.py
+++ test_Sensors.py
@@ -3,12 +3,14 @@ import time
 
 distanceSensor = Sensor(USTriggerPin=17, USEchoPin=18)
 LineTracker1 = Sensor(PinIR=25)
+LineTracker2 = Sensor(PinIR=23)
 
 try:
     # Repeat the next indented block forever
     while True:
         print "Distance: %2.f cm" % float(distanceSensor.MeasureDistance())
-        print "IR sensor detect an object: ", LineTracker1.StatusRead()
+        print "IR-1 sensor detect an object: ", LineTracker1.StatusRead()
+        print "IR-2 sensor detect an object: ", LineTracker2.StatusRead()
         time.sleep(0.1)
 
 # If you press CTRL+C, cleanup and stop
